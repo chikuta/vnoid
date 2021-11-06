f545c1b611a2a63bed0b3973e3a7b45c77729c8b
chikuta <takeshi.chiku@gmail.com>
2021-11-06 17:04:36 +0900

    Fix dockerfile

--- docker/run-docker-container.bash
+++ docker/run-docker-container.bash
@@ -18,5 +18,5 @@ docker run \
   -e XAUTHORITY=$home_folder/.Xauthority \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
-  -it --name "ros-noetic" ${user}/ros-focal-noetic
+  -it --name "focal-noetic-vnoid" ${user}/focal-noetic-vnoid
 
